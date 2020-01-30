cd2ceb25e1354d7662b1e241e217b1a5b5542758
Derek <legato_5@yahoo.com>
2020-01-29 19:03:31 -0800

    finished crud coding. testing to insure they work properly.

--- router/account-router.js
+++ router/account-router.js
@@ -29,13 +29,37 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-
+    const accountData = req.body;
+    try{
+        const account = await db('accounts').insert(accountData);
+        console.log(account)
+        res.status(201).json(account)
+    }
+    catch(err) {
+        res.status(500).json({message: "Failed to create account"})
+    }
 })
 
 router.put('/:id', async (req, res) => {
-
+    const {id} = req.params;
+    try{
+        const updateData = await db('accounts').where('id', id).update(req.body);
+        res.status(200).json({updated: updateData})
+    }
+    catch(err){
+        res.status(500).json({message: "Failed to update account"})
+    }
 })
 
 router.delete('/:id' async (req, res) => {
-
+    const {id} = req.params;
+    try{
+        const deleteAccount = await db('accounts').where('id', id).del();
+        res.status(204).end();
+    }
+    catch(err) {
+        res.status(500).json({message: "Failed to delete account"})
+    }
 })
+
+module.exports = router;
