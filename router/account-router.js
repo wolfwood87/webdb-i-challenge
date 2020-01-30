0c87a6e39de0585cec2d25414cec5bc14e94fa9d
Derek <legato_5@yahoo.com>
2020-01-29 19:30:12 -0800

    all endpoints working correctly. MVP finished

--- router/account-router.js
+++ router/account-router.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const db = require('../data/dbConfig.js');
 
-const router = express.router();
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try{
         const accounts = await db('accounts');
         console.log(accounts);
-        req.status(200).json(accounts)
+        res.status(200).json(accounts);
     }
     catch(err) {
         res.status(500).json({message: "Failed to retrieve accounts"})
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
     const {id} = req.params;
 
     try{
-        const account = await db('acounts').where('id', id);
+        const account = await db('accounts').where('id', id);
         console.log(account);
         res.status(200).json(account);
     }
@@ -51,7 +51,7 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-router.delete('/:id' async (req, res) => {
+router.delete('/:id', async (req, res) => {
     const {id} = req.params;
     try{
         const deleteAccount = await db('accounts').where('id', id).del();
